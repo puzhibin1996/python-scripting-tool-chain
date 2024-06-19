e8ffb1e34ea7b5061770a92bfe4bd510d488bb14
蒲志斌 <1075057376@qq.com>
2024-06-19 22:53:01 +0800

    playwright 框架
    更新访问抖音页面，进行上下滚动加载视频

--- Playwright-Actual-combat/02-标签元素/01-获取当前页面所有标签元素.py
+++ Playwright-Actual-combat/02-标签元素/01-获取当前页面所有标签元素.py
@@ -46,10 +46,51 @@ class BrowserController:
     def fun_screenshot(self):
         if self.page:
             self.page.screenshot(path="img/screenshot.png")
-    #
-    
+    # 打开网页版抖音
+    def fun_douyin(self):
+        if self.page:
+            self.page.goto("https://www.douyin.com/")
 
+    # 对当前页面进行截图
+    def fun_once(self):
+        if self.page:
+            self.page.screenshot(path="img/douyin.png")
+            print("成功截图！")
+        else:
+            print("无法截图，请检查浏览器是否连接成功！")
 
+    # 对当前标签进行点击 //*[@id="login-pannel"]/div[2]
+    def fun_click(self):
+        if self.page:
+            self.page.click("//*[@id=\"login-pannel\"]/div[2]")
+            print("成功点击！")
+        else:
+            print("无法点击，请检查浏览器是否连接成功！")
+
+    # 触发鼠标滚轮 上下滚轮
+    def fun_scroll(self):
+        if self.page:
+            self.page.mouse.wheel(0, 1000)
+            print("成功触发鼠标滚轮！")
+        else:
+            print("无法触发鼠标滚轮，请检查浏览器是否连接成功！")
+    # 当前页面取消禁音状态。 //*[@id="sliderVideo"]/div[1]/div[1]/div/xg-controls/xg-inner-controls/xg-right-grid/xg-icon[3]/div/svg[3]
+    def fun_unmute(self):
+        if self.page:
+            self.page.evaluate("document.querySelector('video').muted=false")
+            print("成功取消禁音状态！")
+        else:
+            print("无法取消禁音状态，请检查浏览器是否连接成功！")
+
+    # 当前视频停留15秒，然后鼠标滚轮滚轮切换下面一条视频
+    def fun_scroll_next(self):
+        if self.page:
+            self.page.wait_for_timeout(15000)
+            self.page.mouse.wheel(0, 1000)
+            self.page.mouse.wheel(1000, 2000)
+            print("成功触发鼠标滚轮！")
+        else:
+            print("无法触发鼠标滚轮，请检查浏览器是否连接成功！")
 
 
 if __name__ == '__main__':
@@ -62,5 +103,11 @@ if __name__ == '__main__':
     controller.connect_to_browser("https://www.baidu.com")
 
     # controller.get_all_elements()
-    controller.fun_screenshot()
+    # controller.fun_screenshot()
     # controller.fun_once()
+    controller.fun_douyin()
+    controller.fun_once()
+    # controller.fun_click()
+    controller.fun_scroll()
+    controller.fun_unmute()
+    controller.fun_scroll_next()
