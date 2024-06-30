52af14b1feb451d65a46d9b54026aadac164a376
蒲志斌 <1075057376@qq.com>
2024-06-30 20:44:46 +0800

    appium框架
    更新appium框架内容

--- appium/appium_01.py
+++ appium/appium_01.py
@@ -1,23 +1,19 @@
-
 import time
 import logging
 from appium.webdriver import Remote
 
-
 # Enable logging
 logging.basicConfig(level=logging.DEBUG)
 
-desired_caps = dict()
-desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = '12'
-desired_caps['deviceName'] = '127.0.0.1:62001'
-desired_caps['appPackage'] = 'com.android.settings'
-desired_caps['appActivity'] = 'com.android.settings.SubSettings'
+desired_caps = {
+    'platformName': 'Android',
+    'platformVersion': '12',
+    'deviceName': '127.0.0.1:62001',
+    'appPackage': 'com.android.settings',
+    'appActivity': 'com.android.settings.SubSettings'
+}
 
 driver = Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
 time.sleep(5)
 driver.quit()
-
-
-
